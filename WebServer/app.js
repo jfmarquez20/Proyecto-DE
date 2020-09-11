77ec7af984c64b53d98ec35b77db960f5810a363
Juan Marquez <jfmarquez@uninorte.edu.co>
2020-09-10 21:48:11 -0500

    feat: Backend with Express, API Google Maps, CSS

--- WebServer/app.js
+++ WebServer/app.js
@@ -1,25 +1,25 @@
 var fs = require('fs');
-var http = require('http');
 var dgram = require('dgram');
 var socketio = require('socket.io');
+var express = require('express');
 
-var app = http.createServer(handleRequest);
-var io = socketio.listen(app);
+var app = express();
+var server = require('http').Server(app);       
+var io = socketio.listen(server);
 var socket = dgram.createSocket('udp4');
 
-socket.on('message', (content, rinfo) => {
+socket.on('message', (content, rinfo) => { 
     console.log(`Server got: ${content} from ${rinfo.address}:${rinfo.port}`);
     io.sockets.emit('udp message', content.toString());
 });
 
-function handleRequest (req, res) {
-    res.writeHead(200, {'content-type': 'text/html'});
+app.get('/', (request, response) => {
+    response.writeHead(200, {'content-type': 'text/html'});
     var file = fs.createReadStream('index.html');
-    file.pipe(res);
-}
+    file.pipe(response);
+  });
 
 socket.bind(50000);
-app.listen(15002, function (){
+server.listen(15002, () => {
     console.log("Servidor abierto en puerto 15002");
-}
-);
+});
